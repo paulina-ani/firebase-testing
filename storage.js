0da246bef3110a30543378dc500111bf81152432
Paulina <aniskiewicz.paulina@gmail.com>
2019-08-18 12:34:29 +0200

    Functions and methods for avatar images added

--- storage.js
+++ storage.js
@@ -1,17 +1,69 @@
-//Reference to firebase
-var storage = firebase.storage();
-
 //Variable with the file
 var selectedFile;
+var fileInput = document.getElementById("file"); //input
+
+fileInput.addEventListener("change", function(event) {
+  selectedFile = event.target.files[0];
+});
 
 //Reference to profile image
-var avatar = document.getElementById("avatar");
+var avatar = document.getElementById("avatar"); //img
+var changeImg = document.getElementById("change-file"); //button
+var deleteImg = document.getElementById("delete-avatar"); //button
 
 //Add new image
-function uploadFile() {}
+function uploadFile() {
+  var userData = getUserData();
+  return firebase
+    .storage()
+    .ref("avatars/" + userData.uid)
+    .put(selectedFile)
+    .then(function() {
+      alert("Dodano avatar");
+    })
+    .catch(function(error) {
+      alert("Error " + error.message);
+    });
+}
 
 // Delete the image
-function deleteAvatar() {}
+function deleteAvatar() {
+  var userData = getUserData();
+  return firebase
+    .storage()
+    .ref("avatars/" + userData.uid)
+    .delete()
+    .then(function() {
+      alert("Usunięto pomyślnie");
+    });
+}
 
 // Download the image
-function getAvatar() {}
+function getAvatar() {
+  var userData = getUserData();
+  return firebase
+    .storage()
+    .ref("avatars/" + userData.uid)
+    .getDownloadURL()
+    .then(function(url) {
+      avatar.serAttribute("src", url);
+    })
+    .catch(avatar.setAttribute("src", "avatar.jpg"));
+}
+
+deleteImg.addEventListener("click", function() {
+  deleteAvatar().then(function() {
+    getAvatar();
+  });
+});
+
+changeImg.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (selectedFile) {
+    uploadFile().then(function() {
+      getAvatar();
+      selectedFile = null;
+    });
+  }
+});
